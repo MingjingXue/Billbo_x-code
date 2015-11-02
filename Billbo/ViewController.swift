ccdb7e12d40ffbdb36b2615e166256f35ac30b2f
MINGJING XUE <xuemingjing@xuedemacbook-pro.local>
2015-11-02 14:25:06 -0800

    Initial Commit

---
+++ Billbo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Billbo
+//
+//  Created by XUE MINGJING on 11/2/15.
+//  Copyright © 2015 XUE MINGJING. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
